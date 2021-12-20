5baf37e8ffdb82f05a40131f30a0346b91047c0b
i552011 <jan.bertleff@gmail.com>
2021-12-20 22:38:46 +0100

    async function working

--- src/Components/Auswahl/Overview.tsx
+++ src/Components/Auswahl/Overview.tsx
@@ -3,8 +3,22 @@ import "../../App.css";
 import "../../index.css";
 import { Card, Space, Row, Col } from "antd";
 import MyRow from "../MyRow";
+import { useState } from "react";
+import axios from "axios";
 
 function Overview(props: React.HTMLAttributes<HTMLDivElement>) {
+  const [science, setScience] = useState<string>("");
+  async function getData() {
+    if (science) {
+      return;
+    }
+    const result = await axios.get(
+      "http://localhost:49892/service-course/overview"
+    );
+    setScience(result.data.value);
+    return result;
+  }
+  getData();
   return (
     <>
       <Space align="center" direction="vertical">
@@ -18,6 +32,7 @@ function Overview(props: React.HTMLAttributes<HTMLDivElement>) {
               "Sozialwissenschaften",
               "Wirtschaftswissenschaften",
               "Sonstige",
+              science,
             ],
           }}
         ></MyRow>
